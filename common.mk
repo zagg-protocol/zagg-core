b9e39747b9eb1e67cbd7f1e376cd043496b598a7
vivek yarra <Vivek@metapointer.com>
2019-02-11 16:51:28 +0530

    checkins

--- common.mk
+++ common.mk
@@ -3,7 +3,9 @@
 # AM_CPPFLAGS = -DSQLITE_OMIT_LOAD_EXTENSION=1
 AM_CPPFLAGS = -isystem "$(top_srcdir)" -I"$(top_srcdir)/src" -I"$(top_builddir)/src"
 AM_CPPFLAGS += $(libsodium_CFLAGS) $(xdrpp_CFLAGS) $(libmedida_CFLAGS)	\
-	$(soci_CFLAGS) $(sqlite3_CFLAGS) $(libasio_CFLAGS)
+	$(soci_CFLAGS) $(sqlite3_CFLAGS) $(libasio_CFLAGS) 
+	
+#$(libsecp256k1_LIBS) $(libbitcoin_crypto_base_LIBS) 
 
 # $(libunivalue_CFLAGS)
 
@@ -13,7 +15,10 @@ AM_CPPFLAGS += -isystem "$(top_srcdir)/lib"			\
 	-isystem "$(top_srcdir)/lib/util"			\
 	-isystem "$(top_srcdir)/lib/soci/src/core"	\
 	-isystem "$(top_srcdir)/lib/bitcoin/src" \
-	-isystem "$(top_srcdir)/lib/bitcoin/src/univalue/include"
+	-isystem "$(top_srcdir)/lib/bitcoin/src/univalue/include" \
+	-isystem "$(top_srcdir)/lib/bitcoin/src/secp256k1/include" \
+	-isystem "$(top_srcdir)/lib/bitcoin/src/crypto"
+
 	
 #  \
 # -isystem "/usr/include/boost/"
