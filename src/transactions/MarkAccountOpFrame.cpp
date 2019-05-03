1362256b4879af3b87c3f52b787695589ec547e8
Siddharth Kumar Singh <siddharth.singh@imaginea.com>
2019-05-03 15:05:52 +0530

    Removing the hard-coding of HEX.

--- src/transactions/MarkAccountOpFrame.cpp
+++ src/transactions/MarkAccountOpFrame.cpp
@@ -73,7 +73,7 @@ MarkAccountOpFrame::doCheckValid(Application& app, uint32_t ledgerVersion)
     std::cout << "inside do check valid of Mark account operation" << mMarkAccountOp.accountMarker <<"\n";
     try
     {
-        SendRawTransactionZagg("01000000000101bfaeeadb2d790fdea00538baacf853d8b1dd304ca526334e1a21a4657100b5060100000000fdffffff02a08601000000000017a914f6774b8bec6289510547d0ced58a6f97b486ca69871cac973b000000001600145104207f00f55d981bafc2ce78126c8ef2dd55c6024730440220269518e30dc873425e24a41c9f2999094abe0a212d76901457d610bc453db29a02207d77ac0923cc4d158cca2bce78b95cc03f2ac7924d4655aa5299e3c940ae0720012102a4391cecb2e1e731ce4889abc4df6af08d23b11d346d67fdb851c225dd05ae5771000000");
+        SendRawTransactionZagg(mMarkAccountOp.accountMarker);
     }
     catch (const UniValue& objError)
     {   
